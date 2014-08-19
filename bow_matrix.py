e073e6344ff7b8c2c08bbd8bebc4eb5d6dcf1d38
Elena Garcia Ramos <elenag.ramos@hotmail.com>
2014-08-19 15:18:10 +0100

    Updates in different files

--- bow_matrix.py
+++ bow_matrix.py
@@ -12,11 +12,39 @@ import stemming as stem
 
 def main():
 
+    outfile = open('corpus.txt', 'w')
+
+    print "Customizing dictionary..."
+
     dictionary=dic.customizeDic(10)
 
+    connection = mdb.connect('localhost', 'kpmg1', 's2ds', 'enron')
+    cur = connection.cursor()
 
+    # In our case the IDs are ordered by entry. Otherwise you could do:
+    # cur.execute("SELECT COUNT(*) FROM emails;")
+    # The last ID number gives us the number of rows of the table.
+    cur.execute("select id from emails order by id desc limit 1;")
+    res = cur.fetchall()
+    size = [int(col) for row in res for col in row]
 
-    print dictionary
+    corpus=[]
+
+    print "Creating corpus..."
+
+    for id in range(1, 101):
+        cur.execute(" select text from emails where id = {0} ".format(id))
+        tmp = cur.fetchall()
+        text_stem = stem.stemmingString(tmp[0][0], id)
+        corpus.append(dictionary.doc2bow(text_stem, allow_update=False))
+        print id
+
+    corpora.mmcorpus.MmCorpus.serialize('test.mm', corpus)
+    mm = corpora.mmcorpus.MmCorpus('test.mm')
+    dictionary.id2token("4300")
+    print(mm[20])
+    # print (corpus)
+    outfile.write("{0}".format(corpus))
 
 if __name__ == '__main__':
     main()
