dcffb0b27ef12d5254f9f734c50cfb782f88b412
Elena Garcia Ramos <elenag.ramos@hotmail.com>
2014-08-19 19:30:28 +0100

    memory-friendly create_corpus using iterators

--- create_corpus.py
+++ create_corpus.py
@@ -11,18 +11,20 @@ import stemming as stem
 import enron
 
 
-class MyCorpus(object):
+class MyCorpus():
     def __iter__(self):
         try:
+            dictionary=dic.customizeDic(10)
             connection = mdb.connect('localhost', 'kpmg1', 's2ds', 'enron')
             cur = connection.cursor()
             cur.execute("select id from emails order by id desc limit 1;")
             res = cur.fetchall()
             size = [int(col) for row in res for col in row]
-            for i in range(1,size[0]):
-                command="""'emails','text','id = {0}'""".format(i)
-                text = enron.queryDb(command)[0][0]
-                text_stem = stem.stemmingString(text, id)
+            for id in range(1,size[0]):
+                print id
+                cur.execute(" select text from emails where id = {0} ".format(id))
+                tmp = cur.fetchall()
+                text_stem = stem.stemmingString(tmp[0][0], id)
                 yield dictionary.doc2bow(text_stem, allow_update=False)
         finally:
             connection.close()
@@ -33,26 +35,30 @@ def main():
 
     print "Customizing dictionary..."
 
-    dictionary=dic.customizeDic(10)
 
-    connection = mdb.connect('localhost', 'kpmg1', 's2ds', 'enron')
-    cur = connection.cursor()
-
+    #
+    # connection = mdb.connect('localhost', 'kpmg1', 's2ds', 'enron')
+    # cur = connection.cursor()
+    #
     # In our case the IDs are ordered by entry. Otherwise you could do:
     # cur.execute("SELECT COUNT(*) FROM emails;")
     # The last ID number gives us the number of rows of the table.
-    cur.execute("select id from emails order by id desc limit 1;")
-    res = cur.fetchall()
-    size = [int(col) for row in res for col in row]
+    # cur.execute("select id from emails order by id desc limit 1;")
+    # res = cur.fetchall()
+    # size = [int(col) for row in res for col in row]
 
     print "Creating corpus..."
 
     corpus=MyCorpus()
 
-    outfile.write("{0}".format(corpus))
+    # outfile.write("{0}".format(corpus))
     corpora.mmcorpus.MmCorpus.serialize('corpus.mm', corpus)
+    mm = corpora.mmcorpus.MmCorpus('corpus.mm')
+    print mm[9]
+    #
+    # connection.close()
 
-    connection.close()
+    return corpus
 
 if __name__ == '__main__':
     main()
