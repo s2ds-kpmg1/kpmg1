08d7f0d050cfefdebffed0a9173e0f3a48c1f7e5
Elena Garcia Ramos <elenag.ramos@hotmail.com>
2014-08-21 09:55:44 +0100

    Some updates

--- create_corpus.py
+++ create_corpus.py
@@ -14,14 +14,14 @@ import enron
 class MyCorpus():
     def __iter__(self):
         try:
-            dictionary=dic.customizeDic(10)
+            print "Customizing dictionary..."
+            dictionary=dic.customizeDic(10,30000)
             connection = mdb.connect('localhost', 'kpmg1', 's2ds', 'enron')
             cur = connection.cursor()
             cur.execute("select id from emails order by id desc limit 1;")
             res = cur.fetchall()
             size = [int(col) for row in res for col in row]
             for id in range(1,size[0]):
-                print id
                 cur.execute(" select text from emails where id = {0} ".format(id))
                 tmp = cur.fetchall()
                 text_stem = stem.stemmingString(tmp[0][0], id)
@@ -33,30 +33,11 @@ def main():
 
     outfile = open('corpus.txt', 'w')
 
-    print "Customizing dictionary..."
-
-
-    #
-    # connection = mdb.connect('localhost', 'kpmg1', 's2ds', 'enron')
-    # cur = connection.cursor()
-    #
-    # In our case the IDs are ordered by entry. Otherwise you could do:
-    # cur.execute("SELECT COUNT(*) FROM emails;")
-    # The last ID number gives us the number of rows of the table.
-    # cur.execute("select id from emails order by id desc limit 1;")
-    # res = cur.fetchall()
-    # size = [int(col) for row in res for col in row]
-
     print "Creating corpus..."
 
     corpus=MyCorpus()
 
-    # outfile.write("{0}".format(corpus))
     corpora.mmcorpus.MmCorpus.serialize('corpus.mm', corpus)
-    mm = corpora.mmcorpus.MmCorpus('corpus.mm')
-    print mm[9]
-    #
-    # connection.close()
 
     return corpus
 
