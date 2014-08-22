99046ac0d458074d47c346573e095be6b1d0349e
Emma Walker <emma.s.walker@gmail.com>
2014-08-22 14:16:10 +0100

    lda testing
    something weird happened to this and it reverted back to an old version

--- lda_analysis.py
+++ lda_analysis.py
@@ -2,18 +2,41 @@
 
 import logging, gensim, bz2
 import pdb
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+import random
+    
+    
+def main():
+    
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    
+    # load id->word mapping (the dictionary), one of the results of step 2 above
+    id2word = gensim.corpora.Dictionary.load_from_text('dictionary_words.txt')
+    # load corpus iterator
+    mm = gensim.corpora.MmCorpus('corpus.mm')
+    # mm = gensim.corpora.MmCorpus(bz2.BZ2File('wiki_en_tfidf.mm.bz2')) # use this if you compressed the TFIDF output
+    
+    print(mm)
 
-# load id->word mapping (the dictionary), one of the results of step 2 above
-id2word = gensim.corpora.Dictionary.load_from_text('dictionary_words.txt')
-# load corpus iterator
-mm = gensim.corpora.MmCorpus('corpus.mm')
-# mm = gensim.corpora.MmCorpus(bz2.BZ2File('wiki_en_tfidf.mm.bz2')) # use this if you compressed the TFIDF output
+    #we can split the corpus to test the samples with a hold-out technique
 
-print(mm)
+    cp = list(mm)
+    random.shuffle(cp)
 
-lda = gensim.models.ldamodel.LdaModel(corpus=mm, id2word=id2word, num_topics=100, update_every=1, chunksize=5000, passes=5)
+    cp_size = int(len(cp)*0.8)
+    cp_train = cp[0:p]
+    cp_test = cp[p:]
 
-pdb.set_trace()
 
-lda.print_topics(20)
+    
+    #This is the pure gensim version. It uses variational Bayes
+    #lda = gensim.models.ldamodel.LdaModel(corpus=mm, id2word=id2word, num_topics=100, update_every=1, chunksize=5000, passes=5)
+    
+    pdb.set_trace()
+    
+    #This is the same set-up but run through the MALLET code instead which uses Gibbs sampling
+    lda_mallet = gensim.models.mallet('/Users/emma/mallet/bin/mallet', corpus=mm, id2word=id2word, num_topics=100, update_every=1, chunksize=5000, passes=5)
+
+    
+if __name__ == "__main__":
+
+    main()
